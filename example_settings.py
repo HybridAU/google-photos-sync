fba59d001830577ea13414a5d13c2dcf3fb625b6
Michael Van Delft <michael@hybr.id.au>
2018-12-15 12:37:56 +0800

    Download a file
    The code still does basicly nothing, but now I can download a single
    file. I'm not sure if I'm doing it the right way but it seems ok.
    
    I still need to work on making it object oriented and dealing with
    access tokens when they expire but my next goal is to see if I can
    upload a file.

--- example_settings.py
+++ example_settings.py
@@ -3,6 +3,8 @@ An example settings file, simply copy this file to settings.py
 and fill in the details
 """
 
+OAUTH_2_ACCESS_TOKEN = ""
+OAUTH_2_REFRESH_TOKEN = ""
+OAUTH_2_TOKEN_URI = "https://oauth2.googleapis.com/token"
 OAUTH_2_CLIENT_ID = ""
 OAUTH_2_CLIENT_SECRET = ""
-OAUTH_2_ACCESS_TOKEN = ""
--- google_photos_sync.py
+++ google_photos_sync.py
@@ -1,6 +1,7 @@
 """
 Sync Google Photos with local storage
 """
+import json
 
 import google.oauth2.credentials
 import google.auth.transport.requests
@@ -8,9 +9,28 @@ import google.auth.transport.requests
 import settings
 
 credentials = google.oauth2.credentials.Credentials(
-    settings.OAUTH_2_ACCESS_TOKEN)
+    settings.OAUTH_2_ACCESS_TOKEN,
+    refresh_token=settings.OAUTH_2_REFRESH_TOKEN,
+    token_uri=settings.OAUTH_2_TOKEN_URI,
+    client_id=settings.OAUTH_2_CLIENT_ID,
+    client_secret=settings.OAUTH_2_CLIENT_SECRET)
 
 authed_session = google.auth.transport.requests.AuthorizedSession(credentials)
 media_items = authed_session.get('https://photoslibrary.googleapis.com/v1/mediaItems')
 
-print(media_items.text)
+# All we are doing here is donloading the latest file, it's pretty crude and
+# it won't work if the latest media item is a video but it's baby steps
+# towards something useful.
+
+latest_media = json.loads(media_items.text)
+latest_photo_url = latest_media['mediaItems'][0]['baseUrl']
+latest_photo_width = latest_media['mediaItems'][0]['mediaMetadata']['width']
+latest_photo_file_name = latest_media['mediaItems'][0]['filename']
+
+# We don't actually have to use an authenticated session here, we could just
+# use the standard requests library to pull it down.
+photo = authed_session.get(latest_photo_url + "=w" + latest_photo_width)
+
+file = open(latest_photo_file_name, 'wb')
+file.write(photo.content)
+file.close()
